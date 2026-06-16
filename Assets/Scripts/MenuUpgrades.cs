6cd56f6eec301c93de55b229ee9324258cb21164
Zespier <pabloreco.redondo@gmail.com>
2026-06-16 17:33:51 +0200

    Update MenuUpgrades.cs

--- Assets/Scripts/MenuUpgrades.cs
+++ Assets/Scripts/MenuUpgrades.cs
@@ -48,7 +48,7 @@ public class MenuUpgrades : MonoBehaviour {
         Vector3 center = Vector3.zero;
         Vector2 size = upgradeButtons[0].rectTransform.sizeDelta;
 
-        //Los puedo poner como en zigzag por filas, para que parezca una especie de tablero rollo hexagonal, y luego ya mover las weas más a mano si quiero.
+        //Realmente hablamos de hacer lo de las constelaciones para las mejoras, que eso es la focking polla. Puedo coger el Controller de Skuld, y hacer que se mueva por constellations. Lo que puedo automatizar, es donde van las mejoras por el precio, pero la colocación la voy a hacer a mano, porque quiero que tenga forma de constelaciones reales.
     }
 
     public int GetUpgradeLevel(UpgradeType type) {
